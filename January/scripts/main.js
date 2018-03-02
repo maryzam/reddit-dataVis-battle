1f765b0edb9f13163172f07c755dd31c9496839b
maryzam <zamo.code@gmail.com>
2018-03-03 01:38:48 +0300

    Show growth data on half-radial diagram

--- January/scripts/main.js
+++ January/scripts/main.js
@@ -7,12 +7,12 @@ var svg = container.append("svg")
 				.attr("width", width)
 				.attr("height", height);
 
-var innerRadius = 180,
-    outerRadius = Math.min(width, height) * 0.8;
+var innerRadius = Math.min(width, height) * 0.4,
+    outerRadius = Math.min(width, (height-10));
 
 var chart = svg
             .append("g")
-            .attr("transform", "translate(" + width / 2 + "," + height + ")");
+            .attr("transform", "translate(" + width / 2 + "," + (height - 10) + ")");
 
 d3.json("data/algae-growth.json", function (error, data) {
 	if (error) throw error;
@@ -32,13 +32,13 @@ d3.json("data/algae-growth.json", function (error, data) {
 	});
 
 	var scaleTemperature = d3.scaleBand()
-            .range([-Math.PI/2, Math.PI/2])
+            .range([-Math.PI / 2, Math.PI / 2])
             .domain(temperatures)
             .padding(0.01)
             .align(0);
 
 	var scaleSpecies = d3.scaleBand()
-                 .range([0, scaleTemperature.bandwidth()])
+                 .range([0.01, scaleTemperature.bandwidth() - 0.01])
                  .domain(species)
                  .padding(0.1)
                  .align(0);
@@ -49,39 +49,83 @@ d3.json("data/algae-growth.json", function (error, data) {
 	var scaleGrowth = d3.scaleLinear()
                  .range([innerRadius, outerRadius])
                  .domain([minGrowth, maxGrowth]);
-    
-	var tempRadiusOffset = 15;
-	var tempInnerRadius = scaleGrowth(0);
-	var tempOuterRadius = tempInnerRadius + tempRadiusOffset;
 
-	chart
+    var temperatureScaleRadius = 5;
+
+    // draw growth information
+    var angleOffset = scaleSpecies.bandwidth() / 2;
+    var light5000radius = 5;
+    var ligth2500radius = 3;
+    var ligthOffset = temperatureScaleRadius + light5000radius + 3;
+
+    data.forEach(function(d) {
+        var angle = scaleTemperature(d.temperature) + scaleSpecies(d.name) + angleOffset;
+        // rotate the angle
+        d._cos = Math.sin(angle);
+        d._sin = -Math.cos(angle);
+
+        d._radius_5000 = scaleGrowth(d.light_5000) + ligthOffset * (d.light_5000 > 0 ? 1: -1);
+        d._radius_2500 = scaleGrowth(d.light_2500) + ligthOffset * (d.light_2500 > 0 ? 1: -1);
+        d._supportLine = {
+            x1: d._cos * (innerRadius - ligthOffset*2),
+            y1: d._sin * (innerRadius - ligthOffset*2),
+            x2: d._cos * (outerRadius + ligthOffset*2),
+            y2: d._sin * (outerRadius + ligthOffset*2)
+        };
+    }); 
+
+
+       
+	var growth = chart
+        .append("g")
+            .attr("name", "growth")
+        .selectAll("g")
+            .data(data).enter()
+            .append("g");
+
+    // draw support lines
+   growth
+        .append("line")
+        .attr("x1", function(d) { return d._supportLine.x1; })
+        .attr("y1", function(d) { return d._supportLine.y1; })
+        .attr("x2", function(d) { return d._supportLine.x2; })
+        .attr("y2", function(d) { return d._supportLine.y2; })
+        .style("stroke", "#dadada");
+
+    // place core info
+    growth
+        .append("circle")
+        .attr("cx", function(d) { return d._cos * d._radius_5000; })
+        .attr("cy", function(d) { return d._sin * d._radius_5000; })
+        .attr("r", light5000radius)
+        .style("fill", function (d) { return scaleSpeciesColor(d.name); })
+        .style("fill-opacity", 0.7);
+
+    growth
+        .append("circle")
+        .attr("cx", function(d) { return d._cos * d._radius_2500; })
+        .attr("cy", function(d) { return d._sin * d._radius_2500; })
+        .attr("r", ligth2500radius)
+        .style("fill", function (d) { return scaleSpeciesColor(d.name); })
+        .style("fill-opacity", 0.7);
+
+    // draw temparature scale
+    var temperatureInnerRadius = scaleGrowth(0) - temperatureScaleRadius;
+    var temperatureOuterRadius = temperatureInnerRadius + (2 * temperatureScaleRadius);
+
+    chart
         .append("g")
             .attr("name", "temperatures")
         .selectAll("path")
             .data(temperatures).enter()
             .append("path")
             .attr("d", d3.arc()
-                        .innerRadius(tempInnerRadius)
-                        .outerRadius(tempOuterRadius)
+                        .innerRadius(temperatureInnerRadius)
+                        .outerRadius(temperatureOuterRadius)
+                        .cornerRadius(3)
                         .startAngle(function (d) { return scaleTemperature(d); })
                         .endAngle(function (d) { return scaleTemperature(d) + scaleTemperature.bandwidth(); }))
             .style("fill", function (d) { return scaleTemperatureColor(d); });
-
-	chart
-        .append("g")
-            .attr("name", "growth")
-        .selectAll("path")
-            .data(data).enter()
-            .append("path")
-            .attr("d", d3.arc()
-                        .innerRadius(tempOuterRadius + 5)
-                        .outerRadius(tempOuterRadius + 15)
-                        .startAngle(function (d) {
-                            d.startAngle = scaleTemperature(d.temperature) + scaleSpecies(d.name);
-                            return d.startAngle;
-                        })
-                        .endAngle(function (d) { return d.startAngle + scaleSpecies.bandwidth(); }))
-            .style("fill", function (d) { return scaleSpeciesColor(d.name); });
 
 });
 
