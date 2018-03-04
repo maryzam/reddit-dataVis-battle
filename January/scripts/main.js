b0b910e1980b10cdb23fb9b1c6bf0fa2db541bd5
maryzam <zamo.code@gmail.com>
2018-03-05 01:21:03 +0300

    Combine summary data and all scales into js objects

--- January/scripts/main.js
+++ January/scripts/main.js
@@ -14,73 +14,109 @@ var svg = container.append("svg")
 var innerRadius = Math.min(width, height) * 0.4,
     outerRadius = Math.min(width, (height));
 
-var chart = svg
-            .append("g")
-            .attr("transform", "translate(" + width / 2 + "," + height + ")");
 
 d3.json("data/algae-growth.json", function (error, data) {
 	if (error) throw error;
 	
-	var temperatures = [];
-	var species = [];
-	var maxGrowth = -Infinity,
-        minGrowth = Infinity;
+    var summary = getAggregatedInfo(data);
+    var scales = prerateAllScales(summary);
 
-	data.forEach(function (d) {
-	    if (temperatures.indexOf(d.temperature) < 0) { temperatures.push(d.temperature); }
-	    if (species.indexOf(d.name) < 0) { species.push(d.name); }
-	    if (maxGrowth < d.light_5000) { maxGrowth = d.light_5000; }
-	    if (maxGrowth < d.light_2500) { maxGrowth = d.light_2500; }
-	    if (minGrowth > d.light_5000) { minGrowth = d.light_5000; }
-	    if (minGrowth > d.light_2500) { minGrowth = d.light_2500; }
-	});
+    showLegend(scales);
+    drawChart(data, scales);
+});
 
-	var scaleTemperature = d3.scaleBand()
-            .range([-Math.PI / 2, Math.PI / 2])
-            .domain(temperatures)
-            .padding(0.01)
-            .align(0);
+function showLegend(scales) {
 
+}
 
-	var scaleGrowth = d3.scaleLinear()
-                 .range([innerRadius, outerRadius])
-                 .domain([minGrowth, maxGrowth]);
+function drawChart(data, scales) {
 
-    // draw growth information
-    data = prepareData(data, species, scaleTemperature, scaleGrowth);
+    data = prepareData(data, scales);
+
+    var chart = svg
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height + ")");
 
     var growthContainers = chart
         .append("g")
-            .attr("name", "growth")
+        .attr("name", "growth")
         .selectAll("g")
-            .data(data).enter()
-            .append("g");
+        .data(data).enter()
+        .append("g");
 
-    drawSupportLines(growthContainers, species);
-    drawGrowthMarkers(growthContainers, species);
+    drawSupportLines(growthContainers);
+    drawGrowthMarkers(growthContainers, scales);
+    drawTemperatureScale(chart, scales);
+}
 
-    drawTemperatureScale(chart, temperatures, scaleTemperature, scaleGrowth);
-});
+function getAggregatedInfo(data) {
+    var temperatures = [];
+    var species = [];
+    var maxGrowth = -Infinity,
+        minGrowth = Infinity;
 
-function prepareData(data, species, scaleTemperature, scaleGrowth) {
+    data.forEach(function (d) {
+        if (temperatures.indexOf(d.temperature) < 0) { temperatures.push(d.temperature); }
+        if (species.indexOf(d.name) < 0) { species.push(d.name); }
+        if (maxGrowth < d.light_5000) { maxGrowth = d.light_5000; }
+        if (maxGrowth < d.light_2500) { maxGrowth = d.light_2500; }
+        if (minGrowth > d.light_5000) { minGrowth = d.light_5000; }
+        if (minGrowth > d.light_2500) { minGrowth = d.light_2500; }
+    });
+
+    return {
+        temperatures: temperatures,
+        species: species,
+        maxGrowth: maxGrowth,
+        minGrowth: minGrowth
+    };
+}
+
+function prerateAllScales(summary) {
+
+    var scaleTemperature = d3.scaleBand()
+        .range([-Math.PI / 2, Math.PI / 2])
+        .domain(summary.temperatures)
+        .padding(0.01)
+        .align(0);
 
     var scaleSpecies = d3.scaleBand()
         .range([tempSeriesOffset, scaleTemperature.bandwidth() - tempSeriesOffset])
-        .domain(species)
+        .domain(summary.species)
         .padding(0.1)
         .align(0);
 
-    var angleOffset = scaleSpecies.bandwidth() / 2;
+    var scaleGrowth = d3.scaleLinear()
+        .range([innerRadius, outerRadius])
+        .domain([summary.minGrowth, summary.maxGrowth]);
+
+    var scaleSpeciesColor = getColorScale(summary.species, d3.interpolateViridis);
+
+    var scaleTemperatureColor = getColorScale(summary.temperatures, d3.interpolateYlOrRd);
+
+    return {
+        temperature: scaleTemperature,
+        temperatureColor: scaleTemperatureColor,
+        species: scaleSpecies,
+        speciesColor: scaleSpeciesColor,
+        growth: scaleGrowth
+    };
+}
+
+function prepareData(data, scales) {
+    
+    var angleOffset = scales.species.bandwidth() / 2;
 
     data.forEach(function (d) {
-        var angle = scaleTemperature(d.temperature) + scaleSpecies(d.name) + angleOffset;
+        var angle = scales.temperature(d.temperature) + scales.species(d.name) + angleOffset;
         // rotate the angle
         d._cos = Math.sin(angle);
         d._sin = -Math.cos(angle);
         d._angle = angle * 180 / Math.PI;
 
-        d._radius_5000 = scaleGrowth(d.light_5000) + ligthOffset * (d.light_5000 > 0 ? 1 : -1);
-        d._radius_2500 = scaleGrowth(d.light_2500) + ligthOffset * (d.light_2500 > 0 ? 1 : -1);
+        d._radius_5000 = scales.growth(d.light_5000) + ligthOffset * (d.light_5000 > 0 ? 1 : -1);
+        d._radius_2500 = scales.growth(d.light_2500) + ligthOffset * (d.light_2500 > 0 ? 1 : -1);
+
         var supportInnerRadius = innerRadius - ligthOffset * 3;
         var supportOuterRadius = outerRadius + ligthOffset * 2;
         d._supportLine = {
@@ -94,29 +130,7 @@ function prepareData(data, species, scaleTemperature, scaleGrowth) {
     return data;
 }
 
-function getColorScale(domain, interpolator) {
-    var helper = d3.scaleSequential(interpolator).domain([-1, domain.length + 1]);
-    var colors = [];
-    for (var i = 0; i < domain.length; i++) {
-        colors.push(helper(i));
-    }
-    return d3.scaleOrdinal().domain(domain).range(colors);
-}
-
-function generateDrop(d) {
-    var x2500 = d._cos * d._radius_2500;
-    var y2500 = d._sin * d._radius_2500;
-    var x5000 = d._cos * d._radius_5000;
-    var y5000 = d._sin * d._radius_5000;
-    var xOffset = -d._sin * 4;
-    var yOffset = d._cos * 4;
-    var coeff = d._radius_2500 > d._radius_5000 ? -2 : 2;
-    return `M ${x5000 + xOffset} ${y5000 + yOffset} 
-            Q ${x5000 + coeff * yOffset} ${y5000 - coeff *xOffset} ${x5000 - xOffset} ${y5000 - yOffset} 
-            L ${x2500 - (coeff / 2) * yOffset} ${y2500 + (coeff / 2) * xOffset} Z`;
-}
-
-function drawSupportLines(growthContainer, species) {
+function drawSupportLines(growthContainer) {
 
     var gradient = svg
         .append("defs")
@@ -143,15 +157,13 @@ function drawSupportLines(growthContainer, species) {
             .style("stroke", "url(#grey-lines)");
 }
 
-function drawGrowthMarkers(growthContainer, species) {
-
-    var scaleSpeciesColor = getColorScale(species, d3.interpolateViridis);
+function drawGrowthMarkers(growthContainer, scales) {
 
     growthContainer
         .append("path")
         .filter(function (d) { return d.light_5000 !== d.light_2500; })
             .attr("d", generateDrop)
-            .style("fill", function (d) { return scaleSpeciesColor(d.name); })
+            .style("fill", function (d) { return scales.speciesColor(d.name); })
             .style("fill-opacity", 1);
 
     growthContainer
@@ -160,16 +172,15 @@ function drawGrowthMarkers(growthContainer, species) {
             .attr("cx", function (d) { return d._cos * d._radius_5000; })
             .attr("cy", function (d) { return d._sin * d._radius_5000; })
             .attr("r", 4)
-            .style("fill", function (d) { return scaleSpeciesColor(d.name); })
+            .style("fill", function (d) { return scales.speciesColor(d.name); })
             .style("fill-opacity", 1);
 }
 
-function drawTemperatureScale(container, temperatures, scaleTemperature, scaleGrowth) {
+function drawTemperatureScale(container, scales) {
 
-    var temperatureInnerRadius = scaleGrowth(0) - temperatureScaleRadius;
+    var temperatureInnerRadius = scales.growth(0) - temperatureScaleRadius;
     var temperatureOuterRadius = temperatureInnerRadius + (2 * temperatureScaleRadius);
-
-    var scaleTemperatureColor = getColorScale(temperatures, d3.interpolateYlOrRd);
+    var temperatures = scales.temperature.domain();
 
     container
         .append("g")
@@ -181,10 +192,32 @@ function drawTemperatureScale(container, temperatures, scaleTemperature, scaleGr
                     .innerRadius(temperatureInnerRadius)
                     .outerRadius(temperatureOuterRadius)
                     .cornerRadius(3)
-                    .startAngle(function (d) { return scaleTemperature(d); })
-                    .endAngle(function (d) { return scaleTemperature(d) + scaleTemperature.bandwidth(); }))
-                .style("fill", function (d) { return scaleTemperatureColor(d); })
+                    .startAngle(function (d) { return scales.temperature(d); })
+                    .endAngle(function (d) { return scales.temperature(d) + scales.temperature.bandwidth(); }))
+                .style("fill", function (d) { return scales.temperatureColor(d); })
                 .style("fill-opacity", 0.8)
                 .style("stroke", "white")
                 .style("stroke-width", "1");
+}
+
+function getColorScale(domain, interpolator) {
+    var helper = d3.scaleSequential(interpolator).domain([-1, domain.length + 1]);
+    var colors = [];
+    for (var i = 0; i < domain.length; i++) {
+        colors.push(helper(i));
+    }
+    return d3.scaleOrdinal().domain(domain).range(colors);
+}
+
+function generateDrop(d) {
+    var x2500 = d._cos * d._radius_2500;
+    var y2500 = d._sin * d._radius_2500;
+    var x5000 = d._cos * d._radius_5000;
+    var y5000 = d._sin * d._radius_5000;
+    var xOffset = -d._sin * 4;
+    var yOffset = d._cos * 4;
+    var coeff = d._radius_2500 > d._radius_5000 ? -2 : 2;
+    return `M ${x5000 + xOffset} ${y5000 + yOffset} 
+            Q ${x5000 + coeff * yOffset} ${y5000 - coeff * xOffset} ${x5000 - xOffset} ${y5000 - yOffset} 
+            L ${x2500 - (coeff / 2) * yOffset} ${y2500 + (coeff / 2) * xOffset} Z`;
 }
