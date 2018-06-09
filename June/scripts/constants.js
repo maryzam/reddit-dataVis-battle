d82f8ad3bb0b3fa92a9de72af868702549470699
maryzam <zamo.code@gmail.com>
2018-06-09 17:26:27 +0300

    Make custom axis grid, improve labels

---
+++ June/scripts/constants.js
@@ -0,0 +1,65 @@
+const riseTypes = [
+  {
+    "name": "Birthright",
+    "position": 1.5
+  },
+  {
+    "name": "Seized Power",
+    "position": 3.5
+  },
+  {
+    "name": "Appointment by Senate",
+    "position": 5
+  },
+  {
+    "name": "Appointment by Army",
+    "position": 6
+  },
+  {
+    "name": "Appointment by Emperor",
+    "position": 7
+  },
+  {
+    "name": "Appointment by Praetorian Guard",
+    "position": 8
+  },
+  {
+    "name": "Purchase",
+    "position": 9
+  },
+  {
+    "name": "Election",
+    "position": 9.5
+  }
+];
+
+const causeTypes = [
+  {
+    "name": "Assassination",
+    "position": 1
+  },
+  {
+    "name": "Natural Causes",
+    "position": 2.5
+  },
+  {
+    "name": "Execution",
+    "position": 4
+  },
+  {
+    "name": "Suicide",
+    "position": 4.75
+  },
+  {
+    "name": "Died in Battle",
+    "position": 5.5
+  },
+  {
+    "name": "Captivity",
+    "position": 6
+  },
+  {
+    "name": "Unknown",
+    "position": 6.5
+  }
+];
--- June/scripts/index.js
+++ June/scripts/index.js
@@ -5,7 +5,7 @@ d3.csv("./data/emperors.v2.csv")
 		const container = d3.select(".container");
 
 		const width = 800;
-		const height = 800;
+		const height = 700;
 		const margin = 100;
 		const svg = container.append("svg")
 						.attr("width", width  + 2 * margin)
@@ -13,16 +13,14 @@ d3.csv("./data/emperors.v2.csv")
 						.append("g")
 						.attr("transform", `translate(${margin},${margin})`);
 
-		const rise = distinct(source, (d) => (d.rise));
-		const cause = distinct(source, (d) => (d.cause));
 		const dynasty = distinct(source, (d) => (d.dynasty));
 
-		const scaleRise = d3.scalePoint()
-							.domain(rise.map((d) => d.name))
+		const scaleRise = d3.scaleLinear()
+							.domain([0, d3.max(riseTypes, (d) => d.position)])
 							.range([0, width]);
 
-		const scaleCause = d3.scalePoint()
-							.domain(cause.map((d) => d.name))
+		const scaleCause = d3.scaleLinear()
+							.domain([0, d3.max(causeTypes, (d) => d.position)])
 							.range([0, height]);
 
 		const scaleDynasty = d3.scaleSequential(d3.interpolateWarm)
@@ -32,38 +30,50 @@ d3.csv("./data/emperors.v2.csv")
 
 		const rises = svg.append("g").attr("class", ".rise-axis")
 						.selectAll(".rise")
-							.data(rise).enter()
+							.data(riseTypes).enter()
 						.append("g")
 							.attr("class", "rise")
-							.attr("transform", (d) => `translate(${scaleRise(d.name)},0)`);
+							.attr("transform", (d) => `translate(${scaleRise(d.position)},0)`);
 
 		rises
 			.append("text")
-			.text((d) => d.name)
-			.attr("transform", `translate(0,${-margin / 2})rotate(-30)`);
+			.attr("transform", `translate(0,${-margin / 2})rotate(-30)`)
+			.selectAll("tspan")
+				.data(splitLabel).enter()
+			.append("tspan")
+				.text((d) => d)
+				.attr("x", 0)
+				.attr("y", 0)
+				.attr("dy", (d, i) => `${(i * 1)}em`);
 
 		rises
 			.append("line")
-			.attr("y2", height)
-			.style("stroke", "grey");
+			.attr("y2", height + margin)
+			.style("stroke", "#333");
 
 		// draw cause axis
 		const causes = svg.append("g").attr("class", ".cause-axis")
 						.selectAll(".cause")
-							.data(cause).enter()
+							.data(causeTypes).enter()
 						.append("g")
 							.attr("class", "cause")
-							.attr("transform", (d) => `translate(0,${scaleCause(d.name)})`);
+							.attr("transform", (d) => `translate(0,${scaleCause(d.position)})`);
 
 		causes
 			.append("text")
-			.text((d) => d.name)
-			.attr("transform", `translate(${-margin / 2},0)rotate(-30)`);
+			.attr("transform", `translate(${-margin / 2},0)rotate(-30)`)
+			.selectAll("tspan")
+				.data(splitLabel).enter()
+			.append("tspan")
+				.text((d) => d)
+				.attr("x", 0)
+				.attr("y", 0)
+				.attr("dy", (d, i) => `${(i * 1)}em`);
 
 		causes
 			.append("line")
-			.attr("x2", width)
-			.style("stroke", "grey");
+			.attr("x2", width + margin )
+			.style("stroke", "#333");
 
 		// show emperors
 
@@ -71,8 +81,14 @@ d3.csv("./data/emperors.v2.csv")
 		const scaleLife = d3.scaleLinear().domain([0, maxAge]).range([0, 25]);
 
 		var simulation = d3.forceSimulation(source)
-		      .force("x", d3.forceX(function(d) { return scaleRise(d.rise); }).strength(1))
-		      .force("y", d3.forceY(function(d) { return scaleCause(d.cause); }).strength(1))
+		      .force("x", d3.forceX(function(d) { 
+		      		const info = riseTypes.find((x) => (x.name === d.rise))
+		      		return scaleRise(info.position); 
+		      	}).strength(1))
+		      .force("y", d3.forceY(function(d) {
+		      		const info = causeTypes.find((x) => (x.name === d.cause))
+		      		return scaleCause(info.position); 
+		      	}).strength(1))
 		      .force("collide", d3.forceCollide().radius((d) => (scaleLife(d["age.death"]) || 0) + 2).iterations(2))
 		      .stop();
 
@@ -90,7 +106,7 @@ d3.csv("./data/emperors.v2.csv")
 				.attr("r", (d) => scaleLife(d["age.death"]))
 				.style("fill", "black")
 				.style("stroke", function (d) {
-					const dynastyId = dynasty.findIndex((x) => (x.name == d.dynasty));
+					const dynastyId = dynasty.indexOf(d.dynasty);
 					return scaleDynasty(dynastyId);
 				})
 				.style("stroke-width", 1);
@@ -99,34 +115,24 @@ d3.csv("./data/emperors.v2.csv")
 			.append("circle")
 				.attr("r", (d) => scaleLife(d["age.reign.end"]))
 				.style("fill", function (d) {
-					const dynastyId = dynasty.findIndex((x) => (x.name == d.dynasty));
+					const dynastyId = dynasty.indexOf(d.dynasty);
 					return scaleDynasty(dynastyId);
 				})
 
 		emperors
 			.append("circle")
 				.attr("r", (d) => scaleLife(d["age.reign.start"]))
-				.style("fill", "balck");
+				.style("fill", "black");
 
 	});
 
-function distinct(source, accessor, ordered = true) {
+function distinct(source, accessor) {
 	const data = {};
 	source.forEach((d) => {
 		const key = accessor(d);
 		data[key] = (data[key] || 0) + 1;
 	});
-	const result = Object.keys(data)
-		.map((d) => ({ 
-			name: d,
-			weight: data[d]
-		}));
-
-	if (ordered) {
-		result.sort((a,b) => (b.weight - a.weight));
-	}
-
-	return result;
+	return Object.keys(data);
 }
 
 function getMax(source, accessor) {
@@ -138,4 +144,8 @@ function getMax(source, accessor) {
 		}
 	}
 	return max;
+};
+
+function splitLabel(label) {
+	return label.name.split(/ (.+)/);
 }
