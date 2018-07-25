9ddb5dd9c9d2cc805db05d06fdca3732fe5b5527
maryzam <zamo.code@gmail.com>
2018-07-26 00:39:49 +0300

    Load feeder info from json file instead of on fly preprocessing

--- July/scripts/app.js
+++ July/scripts/app.js
@@ -1,57 +1,18 @@
 
-	d3.json(" data/birds.json")
-	   .then((source) => {
-
-	   		const feeders = getFeeders(source);
-	   		console.log(feeders);
-    		for (let feederType in feeders) {
-
-    			const feeder = feeders[feederType]
-    			const birds = Object.keys(feeder[0]).filter((x) => (x !== "seed")); 
+	d3.json("data/feeders.json")
+	   .then((feeders) => {
+	   		feeders.forEach((f) => {
+	   			const birds = Object.keys(f.seeds[0])
+	   								.filter((x) => (x !== "seed")); 
     			
     			const stack = d3.stack()
     						.keys(birds)
 	   						.order(d3.stackOrderNone)
     						.offset(d3.stackOffsetNone);
 
-    			const data = stack(feeder);
-    			console.log(feederType);
+    			const data = stack(f.seeds);
+    			console.log(f.feeder);
     			console.log(data);
-    			console.log();
-    		}
-	   });
-
-
-	function getFeeders(birds) {
-		const feeders = {};
-		birds.forEach((b) => {
-			b.feeders.forEach((f) => {
-				if (!feeders[f]) {
-					feeders[f] = [];
-				}
-				feeders[f].push(b);
-			})
-		});
-
-		const seeds = Object.keys(birds[0].seeds);
-
-		for (let feederType in feeders) {
-			const info = [];
-			const feeder = feeders[feederType];
-			seeds.forEach((seed) => {
-				const current = { seed };
-				let total = 0;
-				feeder.forEach((b) => {
-					current[b.bird] = b.seeds[seed];
-					total += b.seeds[seed]; 
-				});			
-				if (total > 0) {
-					info.push(current);
-				}				
-			});
-			feeders[feederType] = info;
-		}
-
-		return feeders;
-	}
-
+	   		});
+	   })
+	   .catch((e) => console.log(e));
